7743358c06bbbf12edea6889d57a2ab5e02bf441
Theo Julienne <theojulienne@github.com>
2021-04-19 13:33:43 +1000

    Adjust USDT provider name to match repo/gem name.

--- ext/tracecap_profiler/probes.d
+++ ext/tracecap_profiler/probes.d
@@ -1,4 +1,4 @@
-provider tracecap_ruby {
+provider tracecap_ruby_profiler {
   probe ruby__sample__std(struct ruby_sample *, const char *);
   probe ruby__sample__fast(struct ruby_sample *, const char *);
 };
